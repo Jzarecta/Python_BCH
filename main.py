1b97bf034c5b6e674caed113bda30f3a0c2edfdc
jzarecta <jza@bitcuners.org>
2020-01-03 17:24:02 -0600

    added scripts and removed backup file

--- main.py
+++ main.py
@@ -4,24 +4,9 @@
 # Help interact with Bitcoin Cash with Python
 # using the bitcash module
 
-# Check balance using a key
 from bitcash import key
 
 key = Key()
 print(key.address)
 print(key.get_balance())
 print(key.get_balance('usd'))
-
-## Send some coins (needs funding previously)
-key.send([( \
-            key.address, \
-            0.1, \
-            'usd'    )])
-
-## Create a WIF (wallet import format)  backup
-print(key.to_wif())
-
-## Recover your MONEY!!
-print('Please put your WIF key?')
-mykey=raw_input()
-recovered_key = Key(mykey)
